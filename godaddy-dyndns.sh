2e0e16e4e2a8f1a3d164ee226dfeecaba42db57f
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2015-07-31 21:52:08 +0200

    Shell script cleanup.

--- godaddy-dyndns.sh
+++ godaddy-dyndns.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-set +x
-OLD_PWD=$PWD
-ROOT_DIR=$(dirname $0)
-cd $ROOT_DIR
-if [ ! -d .venv ] ; then
-  mkdir -p .venv
-  python3 -m venv .venv
+set -e
+
+if [ ! -d venv ]; then
+    python3 -m venv venv
 fi
-source .venv/bin/activate
-pip install --upgrade pif git+git://github.com/saschpe/pygodaddy.git#egg=pygodaddy
-python3 godaddy-dyndns.py
-deactivate
-cd $OLD_PWD
+
+source venv/bin/activate
+pip install -r requirements.txt
+./godaddy-dyndns.py
